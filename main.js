08e95d4b185c98d17ef5ac118ed339298ac72898
PhamDat328 <92577783+PhamDat328@users.noreply.github.com>
2022-11-12 23:12:42 +0700

    Add files via upload

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ const socket = io("https://webrtc-midterm.herokuapp.com/");
 let usernameRemote;
 
 $("#main-content").hide();
-const myVideo = document.getElementById("#localStream");
+const myVideo = document.getElementById("localStream");
 myVideo.muted = true;
 
 socket.on("DANH_SACH_ONLINE", (arrUserInfo) => {
